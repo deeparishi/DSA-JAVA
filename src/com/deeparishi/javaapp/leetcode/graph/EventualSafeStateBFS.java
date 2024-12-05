b125a438627792dcb47da1eafcc80d440a8032c3
deeparishi <addeparishi@gmail.com>
2024-12-05 22:44:19 +0530

    Graph Problems

---
+++ src/com/deeparishi/javaapp/leetcode/graph/EventualSafeStateBFS.java
@@ -0,0 +1,56 @@
+package com.deeparishi.javaapp.leetcode.graph;
+
+//https://leetcode.com/problems/find-eventual-safe-states/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventualSafeStateBFS {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+
+        int row = graph.length;
+        boolean[] visited = new boolean[row];
+        boolean[] pathVisited = new boolean[row];
+        int[] checkmate = new int[row];
+
+        for (int i = 0; i < row; i++) {
+            if(!visited[i])
+                dfs(i, visited, pathVisited, checkmate, graph);
+        }
+        
+        List<Integer> safeNodes =  new ArrayList<>();
+
+        for (int i = 0; i < row; i++) {
+            if(checkmate[i] == 1)
+                safeNodes.add(checkmate[i]);
+        }
+
+        return safeNodes;
+    }
+
+    private boolean dfs(int val, boolean[] visited, boolean[] pathVisited,
+                     int[] checkmate, int[][] graph) {
+
+        visited[val] = true;
+        pathVisited[val] = true;
+
+        for (int neighbour : graph[val]) {
+            if (!visited[neighbour]) {
+                if (dfs(neighbour, visited, pathVisited, checkmate, graph))
+                    return true;
+            } else if (!pathVisited[neighbour]) {
+                return true;
+            }
+        }
+
+        checkmate[val] = 1;
+        pathVisited[val] = false;
+        return false;
+    }
+
+}
